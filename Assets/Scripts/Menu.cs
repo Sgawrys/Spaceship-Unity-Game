4a52efa6a1fcf42ae9b113c5a5acd70f38b1eeb4
Stefan Gawrys <sgawrys@ic.sunysb.edu>
2013-11-25 04:52:54 -0500

    Added main menu GUI before getting into game

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnGUI() {
+		GUI.Window(0, new Rect(0,0,Screen.width,Screen.height),MainMenuFunc,"");
+	}
+	
+	void MainMenuFunc(int windowId) {
+		GUI.Label(new Rect((Screen.width/2)-75,(Screen.height/2)-300,200,100),"Spaceship Explorer");
+		GUI.Label(new Rect((Screen.width/2)-100,(Screen.height/2)-200,200,100),"Made by Vincent Gaviria and Stefan Gawrys");
+		if(GUI.Button(new Rect((Screen.width/2)-100, (Screen.height/2)-100, 200, 200), "Play Game")) {
+			Application.LoadLevel("Space");	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
