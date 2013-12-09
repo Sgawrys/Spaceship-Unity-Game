c81691bcf211c858449350ab37d6c70e5dabba6d
Stefan Gawrys <stefangawrys@gmail.com>
2013-12-09 13:57:01 -0500

    Added sounds into the game

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 
 public class GameOver : MonoBehaviour {
-	
-	GameObject text;
+
+
 	
 	// Use this for initialization
 	void Start () {
-		text = GameObject.FindGameObjectWithTag("GameOverText");
+
 	}
 	
 	// Update is called once per frame
